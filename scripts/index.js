88221ff7668889bde62f9fb7c252a243f54a4540
Bárbara Leão <barbaraleaobsc@gmail.com>
2024-03-13 18:58:06 -0300

    js com parcelamento, ajustei com for

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
+let selectParcelas = document.getElementById('parcelas');
+for (let i = 1; i <= 12; i++) {
+    let option = document.createElement('option');
+    option.value = i;
+    option.textContent = i + 'x';
+    selectParcelas.appendChild(option);
+}
 
+function calcularParcelas() {
+    let valor = parseFloat(document.getElementById('valor').value);
+    let parcelas = parseInt(document.getElementById('parcelas').value);
+
+    let valorParcela = (valor / parcelas).toFixed(2);
+    document.getElementById('resultado').textContent = `Valor de cada parcela em ${parcelas}x: R$ ${valorParcela}`;
+}
